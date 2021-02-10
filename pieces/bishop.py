05b3664e9f03b5a549e62bdcb2369f8b8470868b
Thomas Williams <tefwilliams@icloud.com>
2021-02-10 22:09:57 +0000

    Correct bishop unicodes

--- pieces/bishop.py
+++ pieces/bishop.py
@@ -6,7 +6,7 @@ from piece import Piece
 class Bishop(Piece):
     def __init__(self: Bishop, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.__symbol = '\u2658' if self.color == 'white' else '\u265E'
+        self.__symbol = '\u2657' if self.color == 'white' else '\u265D'
 
     @property
     def symbol(self: Bishop) -> str:
