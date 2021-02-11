d43c43836a0165633135853fd41288ad1e6b1685
Thomas Williams <tefwilliams@icloud.com>
2021-02-11 18:04:50 +0000

    Implent initial movement functionality

--- chess.py
+++ chess.py
@@ -1,4 +1,5 @@
 
+from player import Player
 from coordinates import Coordinates
 from square import Square
 
@@ -47,4 +48,57 @@ def print_row_break(row_length: int):
     dashes_per_column = 4
     print("-" * (row_length + 1) * dashes_per_column)
 
-display_board(board)
+def play(board):
+    player_color = 'white'
+
+    for i in range(1):
+        move(player_color)
+
+    swap_player(player_color)
+    display_board(board)
+
+
+def swap_player(player: Player) -> None:
+    player = 'black' if player == 'white' else 'white'
+
+def move(player: Player) -> None:
+    display_board(board)
+
+    print("%s's turn" %('White' if player == 'white' else 'Black'))
+
+    move_from_coordinates = get_coordinates("Enter which square to move from: ")
+    move_to_coordinates = get_coordinates("Enter which square to move to: ")
+
+
+def get_coordinates(input_text: str) -> Coordinates:
+    while True:
+        try:
+            coordinates = input(input_text).upper()
+
+            if not are_valid_coordinates(coordinates):
+                raise ValueError("Invalid coordinates")
+
+            coordinates_as_indices = format_coordinates(coordinates)
+            return Coordinates(coordinates_as_indices)
+
+        except ValueError as e:
+            print(e)
+
+def are_valid_coordinates(coordinates: str) -> bool:
+    if len(coordinates) != 2:
+        return False
+
+    if coordinates[0] not in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']:
+        return False
+
+    if coordinates[1] not in ['1', '2', '3', '4', '5', '6', '7', '8']:
+        return False
+
+    return True
+            
+def format_coordinates(coordinates: str) -> tuple[int, int]:
+    y_grid_values = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    x_grid_values = ['1', '2', '3', '4', '5', '6', '7', '8']
+
+    return (y_grid_values.index(coordinates[0]), x_grid_values.index(coordinates[1]))
+
---
+++ player.py
@@ -0,0 +1,4 @@
+from typing import Literal
+
+
+Player = Literal['white', 'black']
