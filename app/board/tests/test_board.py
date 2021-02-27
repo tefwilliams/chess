22b7b454d347229d571c4ef1e3a80e2b98d165c4
Thomas Williams <tefwilliams@icloud.com>
2021-02-27 18:49:53 +0000

    Update imports

--- app/board/tests/test_board.py
+++ app/board/tests/test_board.py
@@ -1,5 +1,8 @@
-from src import Pawn, Coordinates
-from tests.data.generate_board import generate_board
+
+from app.pieces import Pawn
+from app.coordinates import Coordinates
+from .data.generate_board import generate_board
+
 
 def test_get_piece_returns_piece_with_specified_coordinates() -> None:
     coordinates = Coordinates((1, 2))
@@ -8,5 +11,3 @@ def test_get_piece_returns_piece_with_specified_coordinates() -> None:
     board = generate_board([piece])
 
     assert board.get_piece(coordinates) == piece
-
-test_get_piece_returns_piece_with_specified_coordinates()
