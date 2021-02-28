9a7daa38ca9a5d7b0a1a51b93076fb2947cf82cb
Thomas Williams <tefwilliams@icloud.com>
2021-02-28 16:01:28 +0000

    Update display board logic

--- app/src/repository/repository.py
+++ app/src/repository/repository.py
@@ -6,35 +6,55 @@ from ...src.board import Board
 
 # TODO - add letters/numbers to other sides
 def display_board(board: Board) -> None:
-    board_dimensions = Board.shape
+    print("\n")
+    print_column_numbers()
+    print_row_break()
 
-    print("\n" + "  ", end = '')
+    for row_number in range(Board.shape.y):
+        print_row_start_letter(row_number)
+        print_row_squares(row_number, board)
+        print_row_end_letter(row_number)
+        print_row_break()
 
-    for i in range(board_dimensions.x):
+    print_column_numbers()
+    print("\n")
+
+def print_row_break() -> None:
+    row_length = Board.shape.x
+    dashes_per_column = 4
+    print("-" * ((row_length + 2) * dashes_per_column - 1))
+
+def print_column_numbers() -> None:
+    print_row_start()
+    row_length = Board.shape.x
+
+    for i in range(row_length):
         column_number = Coordinates.x_grid_values[i]
         print("| %s " % column_number, end = '')
 
+    print_row_end()
+
+def print_row_start() -> None:
+    print("   ", end = '')
+
+def print_row_end() -> None:
     print("| ")
-    print_row_break(board_dimensions.x)
 
-    for i in range(board_dimensions.y):
-        print("%s " % Coordinates.y_grid_values[i], end='')
+def print_row_start_letter(row_number: int) -> None:
+    print(" %s " % Coordinates.y_grid_values[row_number], end='')
 
-        for j in range(board_dimensions.x):
-            coordinates = Coordinates((i, j))
-            piece = board.get_piece(coordinates)
+def print_row_end_letter(row_number: int) -> None:
+    print("| %s" % Coordinates.y_grid_values[row_number])
 
-            symbol = piece.symbol if piece else " "
-            print("| %s " % symbol, end='')
+def print_row_squares(row_number: int, board: Board) -> None:
+    row_length = Board.shape.x
 
-        print("| ")
-        print_row_break(board_dimensions.x)
+    for column_number in range(row_length):
+        coordinates = Coordinates((row_number, column_number))
+        piece = board.get_piece(coordinates)
 
-    print("\n")
-
-def print_row_break(row_length: int):
-    dashes_per_column = 4
-    print("-" * (row_length + 1) * dashes_per_column)
+        symbol = piece.symbol if piece else " "
+        print("| %s " % symbol, end='')
 
 def get_starting_pieces() -> list[Piece]:
     board_dimensions = Board.shape
