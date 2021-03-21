60da2f3744dc6809ff93a528fe8d4543d8708ce8
Thomas Williams <tefwilliams@icloud.com>
2021-03-21 13:47:04 +0000

    Tidy up parameterisation

--- app/tests/board/test_board.py
+++ app/tests/board/test_board.py
@@ -4,26 +4,6 @@ from app.src import PieceTypes, Coordinates, Board, Color
 from app.tests.data.generate_piece import generate_piece
 
 
-def supply_check_returns_true() -> list[tuple[PieceTypes, str]]:
-    return [
-        (PieceTypes.queen, 'A1'),
-        (PieceTypes.queen, 'F4'),
-        (PieceTypes.bishop, 'F2'),
-        (PieceTypes.rook, 'D8'),
-        (PieceTypes.knight, 'B3'),
-        (PieceTypes.pawn, 'E5')
-    ]
-
-def supply_check_returns_false() -> list[tuple[PieceTypes, str]]:
-    return [
-        (PieceTypes.queen, 'A2'),
-        (PieceTypes.queen, 'F3'),
-        (PieceTypes.bishop, 'F8'),
-        (PieceTypes.rook, 'E2'),
-        (PieceTypes.knight, 'B2'),
-        (PieceTypes.pawn, 'E1')
-    ]
-
 def test_get_piece_returns_piece_with_specified_coordinates() -> None:
     piece = generate_piece(PieceTypes.pawn, 'A1', Color.white)
     get_from_coordinates = Coordinates.convert_from_grid_value('A1') 
@@ -40,7 +20,17 @@ def test_get_piece_returns_none_if_no_piece_has_specified_coordinates() -> None:
 
     assert board.get_piece(get_from_coordinates) == None
 
-@pytest.mark.parametrize("black_piece, piece_coordinates", supply_check_returns_true())
+@pytest.mark.parametrize(
+    "black_piece, piece_coordinates", 
+    [
+        (PieceTypes.queen, 'A1'),
+        (PieceTypes.queen, 'F4'),
+        (PieceTypes.bishop, 'F2'),
+        (PieceTypes.rook, 'D8'),
+        (PieceTypes.knight, 'B3'),
+        (PieceTypes.pawn, 'E5')
+    ]
+)
 def test_is_in_check_returns_true_when_king_in_check(black_piece, piece_coordinates) -> None:
     pieces = [
         generate_piece(PieceTypes.king, 'D4', Color.white),
@@ -51,7 +41,17 @@ def test_is_in_check_returns_true_when_king_in_check(black_piece, piece_coordina
 
     assert board.is_in_check(Color.white)
 
-@pytest.mark.parametrize("black_piece, piece_coordinates", supply_check_returns_false())
+@pytest.mark.parametrize(
+    "black_piece, piece_coordinates",
+    [
+        (PieceTypes.queen, 'A2'),
+        (PieceTypes.queen, 'F3'),
+        (PieceTypes.bishop, 'F8'),
+        (PieceTypes.rook, 'E2'),
+        (PieceTypes.knight, 'B2'),
+        (PieceTypes.pawn, 'E1')
+    ]
+)
 def test_is_in_check_returns_false_when_king_not_in_check(black_piece, piece_coordinates) -> None:
     pieces = [
         generate_piece(PieceTypes.king, 'D4', Color.white),
