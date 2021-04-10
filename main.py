60a19edbf18bbeb8693b96023fb9ce90c47facdc
Thomas Williams <tefwilliams@icloud.com>
2021-04-10 14:08:42 +0100

    Integrate new board with existing game logic

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 
 from chess import Game, Color, display_board
-
+from test import Screen
+import pygame
 
 game = Game()
+screen = Screen()
 
-while not game.over():
+while not any(event.type == pygame.QUIT for event in pygame.event.get()) and not game.over():
+    screen.update(game.board)
     game.take_turn()
     game.player.swap_color()
 
@@ -12,7 +15,12 @@ display_board(game.board)
 
 if game.check_mate():
     opposing_player_color = Color.get_opposing_color(game.player.color)
-    print("%s in check mate. %s wins!" % (str(game.player.color.name).capitalize(), str(opposing_player_color.name).capitalize()) + "\n")
+    print("%s in check mate. %s wins!" % (str(game.player.color.name).capitalize(
+    ), str(opposing_player_color.name).capitalize()) + "\n")
 
 elif game.stale_mate():
-    print("%s in stale mate. It's a draw!" % str(game.player.color.name).capitalize() + "\n")
+    print("%s in stale mate. It's a draw!" %
+          str(game.player.color.name).capitalize() + "\n")
+
+pygame.quit()
+quit()
--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 
 from __future__ import annotations
+from chess import Board, Piece
 import pygame
 
 
 class Screen:
     board_size = 8
 
-    board_edge_thickness = 30  # px
+    board_edge_thickness = 40  # px
     board_border_thickness = 5  # px
 
     square_size = 60  # px
 
-    white, black, brown = (255, 255, 255), (0, 0, 0), (187, 129, 65)
+    white, black, gray = (255, 255, 255), (0, 0, 0), (180, 180, 180)
 
     size = board_edge_thickness * 2 + \
         board_border_thickness * 4 + square_size * board_size
@@ -19,24 +20,36 @@ class Screen:
     def __init__(self: Screen) -> None:
         pygame.init()
         self.screen = pygame.display.set_mode((Screen.size, Screen.size))
-        self.screen.fill(Screen.black)
+        self.screen.fill(Screen.gray)
 
         self.__create_board_edge()
 
-        self.font = pygame.font.SysFont('Arial', 25)
+        self.text_font = pygame.font.SysFont('arial', 25)
+        self.symbol_font = pygame.font.SysFont('segoeuisymbol', 40)
 
         pygame.display.set_caption("Chess")
 
         icon = pygame.image.load("icons/game_icon.png")
         pygame.display.set_icon(icon)
 
-        [self.__create_square(row_number, column_number)
-         for row_number in range(Screen.board_size) for column_number in range(Screen.board_size)]
-
         self.__label_columns()
         self.__label_rows()
 
+        self.__create_empty_squares()
+
         pygame.display.update()
+
+    def update(self: Screen, board: Board) -> None:
+        self.__create_empty_squares()
+
+        for piece in board.pieces:
+            self.__display_piece(piece)
+
+        pygame.display.update()
+
+    def __create_empty_squares(self: Screen) -> None:
+        [self.__create_empty_square(row_number, column_number)
+         for row_number in range(Screen.board_size) for column_number in range(Screen.board_size)]
 
     def __create_board_edge(self: Screen) -> None:
         outer_margin = Screen.board_border_thickness
@@ -48,12 +61,12 @@ class Screen:
         inner_margin = Screen.board_border_thickness + Screen.board_edge_thickness
         inner_size = Screen.size - inner_margin * 2
 
-        pygame.draw.rect(self.screen, Screen.black, [
+        pygame.draw.rect(self.screen, Screen.gray, [
             inner_margin, inner_margin, inner_size, inner_size])
 
-    def __create_square(self: Screen, row_number: int, column_number: int) -> None:
+    def __create_empty_square(self: Screen, row_number: int, column_number: int) -> None:
         square_color = Screen.white if (
-            row_number + column_number) % 2 == 0 else Screen.black
+            row_number + column_number) % 2 == 0 else Screen.gray
 
         pygame.draw.rect(self.screen, square_color,
                          self.__get_square_parameters(row_number, column_number))
@@ -73,10 +86,11 @@ class Screen:
         for column_number in range(Screen.board_size):
             column_label = column_labels[column_number]
 
-            text = self.font.render(
+            text = self.text_font.render(
                 column_label, True, Screen.black)
 
-            text_width = text.get_rect().width  # TODO - consider calling get_rect() once
+            # TODO - consider calling get_rect() once
+            text_width = text.get_rect().width
             text_height = text.get_rect().height
 
             left_margin = board_margin + \
@@ -85,9 +99,10 @@ class Screen:
 
             top_margin = (board_margin - text_height) / 2
 
+            # TODO - consider naming of bottom_margin (not actually bottom margin)
             bottom_margin = board_margin + \
                 Screen.square_size * Screen.board_size + \
-                top_margin  # TODO - consider naming of right_margin (not actually right margin)
+                top_margin
 
             self.screen.blit(text, (left_margin, top_margin))
             self.screen.blit(text, (left_margin, bottom_margin))
@@ -99,17 +114,19 @@ class Screen:
         for row_number in range(Screen.board_size):
             column_label = row_labels[row_number]
 
-            text = self.font.render(
+            text = self.text_font.render(
                 column_label, True, Screen.black)
 
-            text_width = text.get_rect().width  # TODO - consider calling get_rect() once
+            # TODO - consider calling get_rect() once
+            text_width = text.get_rect().width
             text_height = text.get_rect().height
 
             left_margin = (board_margin - text_width) / 2
 
+            # TODO - consider naming of right_margin (not actually right margin)
             right_margin = board_margin + \
                 Screen.square_size * Screen.board_size + \
-                left_margin  # TODO - consider naming of right_margin (not actually right margin)
+                left_margin
 
             top_margin = board_margin + \
                 Screen.square_size * row_number + \
@@ -118,15 +135,23 @@ class Screen:
             self.screen.blit(text, (left_margin, top_margin))
             self.screen.blit(text, (right_margin, top_margin))
 
+    def __display_piece(self: Screen, piece: Piece) -> None:
+        board_margin = Screen.board_border_thickness * 2 + Screen.board_edge_thickness
+        row_number, column_number = piece.coordinates
 
-Screen()
+        symbol = self.symbol_font.render(
+            piece.symbol, True, Screen.black)
 
-gameExit = False
+        # TODO - consider calling get_rect() once
+        symbol_width = symbol.get_rect().width
+        symbol_height = symbol.get_rect().height
 
-while not gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit = True
+        left_margin = board_margin + \
+            Screen.square_size * column_number + \
+            (Screen.square_size - symbol_width) / 2
 
-pygame.quit()
-quit()
+        top_margin = board_margin + \
+            Screen.square_size * row_number + \
+            (Screen.square_size - symbol_height) / 2
+
+        self.screen.blit(symbol, (left_margin, top_margin))
