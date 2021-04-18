e29c962d1195936f53d35a3eced4cdccdcb3e070
Thomas Williams <tefwilliams@icloud.com>
2021-04-18 19:43:41 +0100

    Move get_starting_piece method to repository

--- app/src/chess/pieces/__init__.py
+++ app/src/chess/pieces/__init__.py
@@ -3,6 +3,5 @@ from .king import King
 from .knight import Knight
 from .pawn import Pawn
 from .piece import Piece, PieceTypes
-from .pieces import Pieces
 from .queen import Queen
 from .rook import Rook
--- app/src/chess/repository/repository.py
+++ app/src/chess/repository/repository.py
@@ -1,7 +1,8 @@
 
-from typing import Callable, Iterable, TypeVar
+from typing import Callable, Iterable, TypeVar, Union
 from ..data import board_size
-from ..pieces import Piece, Pieces
+from ..pieces import Piece, Pawn, Rook, Knight, Bishop, Queen, King
+from ..player import Color
 from ..coordinates import Coordinates
 
 
@@ -11,12 +12,35 @@ def get_starting_pieces() -> list[Piece]:
     for i in range(board_size):
         for j in range(board_size):
             coordinates = Coordinates(i, j)
-            starting_piece = Pieces.get_starting_piece(coordinates)
+            starting_piece = get_starting_piece(coordinates)
 
             if starting_piece:
                 pieces.append(starting_piece)
 
     return pieces
+
+
+def get_starting_piece(coordinates: Coordinates) -> Union[Piece, None]:
+    color = Color.white if coordinates.y in [0, 1] else Color.black
+
+    if coordinates.y in [1, 6]:
+        return Pawn(coordinates, color)
+
+    if coordinates.y in [0, 7]:
+        if coordinates.x in [0, 7]:
+            return Rook(coordinates, color)
+
+        if coordinates.x in [1, 6]:
+            return Knight(coordinates, color)
+
+        if coordinates.x in [2, 5]:
+            return Bishop(coordinates, color)
+
+        if coordinates.x == 3:
+            return Queen(coordinates, color)
+
+        if coordinates.x == 4:
+            return King(coordinates, color)
 
 
 T = TypeVar('T')
