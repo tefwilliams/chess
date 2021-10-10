474b2f1582e70264f53fab08e99516f6b18a1106
Thomas Williams <tefwilliams@icloud.com>
2021-10-10 20:17:01 +0100

    fix bug with magnitude calculation

--- app/chess/grid/step.py
+++ app/chess/grid/step.py
@@ -2,11 +2,13 @@
 from __future__ import annotations
 from .grid import Grid
 
+
 class Step(Grid):
 
     @property
     def has_magnitude_one(self: Step) -> bool:
-        return sum(self) == 1
+        return (abs(self.x) + abs(self.y)) ** 0.5 == 1
+
 
 unit_step_up = Step((-1, 0))
 unit_step_right = Step((0, 1))
@@ -19,6 +21,7 @@ vertical_unit_steps = [unit_step_up, unit_step_down]
 
 orthogonal_unit_steps = horizontal_unit_steps + vertical_unit_steps
 
-diagonal_unit_steps = [vertical_step + horizontal_step for vertical_step in vertical_unit_steps for horizontal_step in horizontal_unit_steps]
+diagonal_unit_steps = [
+    vertical_step + horizontal_step for vertical_step in vertical_unit_steps for horizontal_step in horizontal_unit_steps]
 
 all_unit_steps = orthogonal_unit_steps + diagonal_unit_steps
