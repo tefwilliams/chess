55fbb0c598778bfee6c43d0536a256deb61d0776
Tom Williams <tefwilliams@icloud.com>
2024-07-22 13:25:05 +0100

    Fix pawn tests

--- app/tests/pieces/test_pawn.py
+++ app/tests/pieces/test_pawn.py
@@ -1,106 +1,116 @@
 import pytest
-from chess import Board, Color, PieceType, MovablePiece
+from chess import Color, PieceType, Piece
 from ..repository import (
-    create_pieces,
-    to_coordinates,
+    create_board,
     get_possible_destinations,
     create_move,
 )
 
 
 def test_pawn_can_move_one_or_two_forward_if_it_has_not_moved() -> None:
-    pawn = create_pieces(PieceType.Pawn, "A2", Color.White)
+    board = create_board({"A2": Piece(PieceType.Pawn, Color.White)})
 
-    assert get_possible_destinations(pawn, Board({pawn})) == {
-        to_coordinates("A3"),
-        to_coordinates("A4"),
-    }
+    assert get_possible_destinations("A2", board) == sorted(
+        [
+            "A3",
+            "A4",
+        ]
+    )
 
 
 def test_pawn_can_only_move_one_forward_if_it_has_moved() -> None:
-    pawn = create_pieces(PieceType.Pawn, "A2", Color.White)
+    board = create_board({"A2": Piece(PieceType.Pawn, Color.White)})
 
-    board = Board({pawn})
+    pawn_to_a3 = create_move("A2", "A3")
+    board.move(pawn_to_a3)
 
-    pawn_to_A3 = create_move((pawn, "A3"))
-    board.move(pawn_to_A3)
-
-    assert get_possible_destinations(pawn, Board({pawn})) == {
-        to_coordinates("A4"),
-    }
+    assert get_possible_destinations("A3", board) == sorted(
+        [
+            "A4",
+        ]
+    )
 
 
 def test_pawn_can_move_forward_diagonally_if_enemy_piece_there() -> None:
-    pawn = create_pieces(PieceType.Pawn, "A2", Color.White)
-
-    assert to_coordinates("B3") in get_possible_destinations(
-        pawn, Board({pawn, create_pieces(PieceType.Rook, "B3", Color.Black)})
+    board = create_board(
+        {
+            "A2": Piece(PieceType.Pawn, Color.White),
+            "B3": Piece(PieceType.Rook, Color.Black),
+        }
     )
+
+    assert "B3" in get_possible_destinations("A2", board)
 
 
 def test_pawn_can_move_via_en_passant_if_enemy_pawn_has_just_moved_two_squares() -> (
     None
 ):
-    pawn = create_pieces(PieceType.Pawn, "E2", Color.White)
-    enemy_pawn = create_pieces(PieceType.Pawn, "G1", Color.Black)
+    board = create_board(
+        {
+            "E5": Piece(PieceType.Pawn, Color.White),
+            "F7": Piece(PieceType.Pawn, Color.Black),
+        }
+    )
 
-    board = Board({pawn, enemy_pawn})
+    enemy_pawn_to_f5 = create_move("F7", "F5")
+    board.move(enemy_pawn_to_f5)
 
-    pawn_to_E1 = create_move((pawn, "E1"))
-    board.move(pawn_to_E1)
-
-    assert to_coordinates("F1") in get_possible_destinations(pawn, board)
+    assert "F6" in get_possible_destinations("E5", board)
 
 
 def test_pawn_cannot_move_via_en_passant_if_another_piece_has_moved() -> None:
-    pawn = create_pieces(PieceType.Pawn, "E2", Color.White)
-    enemy_pawn = create_pieces(PieceType.Pawn, "G1", Color.Black)
-    other_enempy_piece = create_pieces(PieceType.Bishop, "H5", Color.Black)
+    board = create_board(
+        {
+            "E5": Piece(PieceType.Pawn, Color.White),
+            "F7": Piece(PieceType.Pawn, Color.Black),
+            "H5": Piece(PieceType.Bishop, Color.Black),
+        }
+    )
 
-    board = Board({pawn, enemy_pawn, other_enempy_piece})
+    enemy_pawn_to_f5 = create_move("F7", "F5")
+    board.move(enemy_pawn_to_f5)
 
-    enemy_pawn_to_E1 = create_move((enemy_pawn, "E1"))
-    board.move(enemy_pawn_to_E1)
+    other_enemy_piece_to_g4 = create_move("H5", "G4")
+    board.move(other_enemy_piece_to_g4)
 
-    other_enemy_piece_to_G4 = create_move((other_enempy_piece, "G4"))
-    board.move(other_enemy_piece_to_G4)
-
-    assert to_coordinates("F1") not in get_possible_destinations(pawn, board)
+    assert "F6" not in get_possible_destinations("E5", board)
 
 
 def test_pawn_cannot_move_forward_diagonally_if_fiendly_piece_there() -> None:
-    pawn = create_pieces(PieceType.Pawn, "A2", Color.White)
-
-    assert to_coordinates("B1") not in get_possible_destinations(
-        pawn, Board({pawn, create_pieces(PieceType.Rook, "B1", Color.White)})
+    board = create_board(
+        {
+            "A2": Piece(PieceType.Pawn, Color.White),
+            "B1": Piece(PieceType.Rook, Color.White),
+        }
     )
+
+    assert "B1" not in get_possible_destinations("A2", board)
 
 
 @pytest.mark.parametrize(
     "square_to_move_to, piece_at_destination",
     [
-        ("C4", create_pieces(PieceType.Queen, "C4", Color.Black)),
-        ("B4", create_pieces(PieceType.Rook, "C3", Color.Black)),
+        ("C4", {"C4": Piece(PieceType.Queen, Color.Black)}),
+        ("B4", {"C3": Piece(PieceType.Rook, Color.Black)}),
     ],
 )
 def test_pawn_cannot_move_two_forward_diagonally(
-    square_to_move_to: str, piece_at_destination: MovablePiece
+    square_to_move_to: str, piece_at_destination: dict[str, Piece]
 ) -> None:
-    pawn = create_pieces(PieceType.Pawn, "A2", Color.White)
-
-    assert not to_coordinates(square_to_move_to) in get_possible_destinations(
-        pawn, Board({pawn, piece_at_destination})
+    board = create_board(
+        {"A2": Piece(PieceType.Pawn, Color.White), **piece_at_destination}
     )
+
+    assert not square_to_move_to in get_possible_destinations("A2", board)
 
 
 def test_pawn_cannot_move_backward() -> None:
-    pawn = create_pieces(PieceType.Pawn, "A2", Color.White)
+    board = create_board({"A2": Piece(PieceType.Pawn, Color.White)})
 
-    assert not to_coordinates(
-        "A1") in get_possible_destinations(pawn, Board({pawn}))
+    assert not "A1" in get_possible_destinations("A2", board)
 
 
 def test_pawn_cannot_move_out_of_board() -> None:
-    pawn = create_pieces(PieceType.Pawn, "A8", Color.White)
+    board = create_board({"A8": Piece(PieceType.Pawn, Color.White)})
 
-    assert len(get_possible_destinations(pawn, Board({pawn}))) == 0
+    assert get_possible_destinations("A8", board) == []
